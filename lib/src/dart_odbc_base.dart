5f74f9f694c72eb0dcbe2390b143d2a3d2191e1f
Paul-Creator <paul.kronegger@gmx.at>
2024-09-07 22:10:12 +0200

    connect via connection strings and get table names

--- lib/src/dart_odbc_base.dart
+++ lib/src/dart_odbc_base.dart
@@ -97,6 +97,171 @@ class DartOdbc {
       ..free(cPassword);
   }
 
+  /// Connects to the database using a connection string instead of a DSN.
+  ///
+  /// [connectionString] is the full connection string that provides all necessary
+  /// connection details like driver, server, database, etc.
+  ///
+  /// This method is useful for connecting to data sources like Excel files or text files
+  /// without having to define a DSN.
+  ///
+  /// Throws a [ConnectionException] if the connection fails.
+  Future<void> connectWithConnectionString(String connectionString) async {
+    final pHConn = calloc.allocate<SQLHDBC>(sizeOf<SQLHDBC>());
+    tryOdbc(
+      _sql.SQLAllocHandle(SQL_HANDLE_DBC, _hEnv, pHConn),
+      handle: _hEnv,
+      operationType: SQL_HANDLE_DBC,
+      onException: HandleException(),
+    );
+    _hConn = pHConn.value;
+
+    final cConnectionString =
+        connectionString.toNativeUtf16().cast<UnsignedShort>();
+
+    final outConnectionString =
+        calloc.allocate<UnsignedShort>(1024); // Adjust size as necessary
+    final outConnectionStringLen = calloc.allocate<Short>(sizeOf<Short>());
+
+    tryOdbc(
+      _sql.SQLDriverConnectW(
+        _hConn,
+        nullptr,
+        cConnectionString,
+        SQL_NTS,
+        outConnectionString,
+        1024,
+        outConnectionStringLen,
+        SQL_DRIVER_NOPROMPT,
+      ),
+      handle: _hConn,
+      operationType: SQL_HANDLE_DBC,
+      onException: ConnectionException(),
+    );
+    calloc
+      ..free(pHConn)
+      ..free(cConnectionString)
+      ..free(outConnectionString)
+      ..free(outConnectionStringLen);
+  }
+
+  /// Retrieves a list of tables from the connected database.
+  ///
+  /// Optionally, you can filter the results by specifying [tableName], [catalog],
+  /// [schema], or [tableType]. If these are omitted, all tables will be returned.
+  ///
+  /// Returns a list of maps, where each map represents a table with its name,
+  /// catalog, schema, and type.
+  ///
+  /// Throws a [FetchException] if fetching tables fails.
+  Future<List<Map<String, String>>> getTables({
+    String? tableName,
+    String? catalog,
+    String? schema,
+    String? tableType,
+  }) async {
+    final pHStmt = calloc.allocate<SQLHSTMT>(sizeOf<SQLHSTMT>());
+    tryOdbc(
+      _sql.SQLAllocHandle(SQL_HANDLE_STMT, _hConn, pHStmt),
+      handle: _hConn,
+      onException: HandleException(),
+    );
+    final hStmt = pHStmt.value;
+
+    final cCatalog = catalog?.toNativeUtf16().cast<UnsignedShort>() ?? nullptr;
+    final cSchema = schema?.toNativeUtf16().cast<UnsignedShort>() ?? nullptr;
+    final cTableName =
+        tableName?.toNativeUtf16().cast<UnsignedShort>() ?? nullptr;
+    final cTableType =
+        tableType?.toNativeUtf16().cast<UnsignedShort>() ?? nullptr;
+
+    tryOdbc(
+      _sql.SQLTablesW(
+        hStmt,
+        cCatalog,
+        SQL_NTS,
+        cSchema,
+        SQL_NTS,
+        cTableName,
+        SQL_NTS,
+        cTableType,
+        SQL_NTS,
+      ),
+      handle: hStmt,
+      onException: FetchException(),
+    );
+
+    // Create a map of column names to table data
+    final result = <Map<String, String>>[];
+
+    // Fetch and store the result rows
+    final columnConfig = {
+      'TABLE_CAT': ColumnType(type: SQL_WCHAR, size: 256),
+      'TABLE_SCHEM': ColumnType(type: SQL_WCHAR, size: 256),
+      'TABLE_NAME': ColumnType(type: SQL_WCHAR, size: 256),
+      'TABLE_TYPE': ColumnType(type: SQL_WCHAR, size: 256),
+    };
+
+    while (_sql.SQLFetch(hStmt) == SQL_SUCCESS) {
+      final row = <String, String>{};
+
+      row['TABLE_CAT'] = _getColumnValue(hStmt, 1, columnConfig['TABLE_CAT']!);
+      row['TABLE_SCHEM'] =
+          _getColumnValue(hStmt, 2, columnConfig['TABLE_SCHEM']!);
+      row['TABLE_NAME'] =
+          _getColumnValue(hStmt, 3, columnConfig['TABLE_NAME']!);
+      row['TABLE_TYPE'] =
+          _getColumnValue(hStmt, 4, columnConfig['TABLE_TYPE']!);
+
+      result.add(row);
+    }
+
+    // Clean up
+    _sql.SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
+    calloc
+      ..free(pHStmt)
+      ..free(cCatalog)
+      ..free(cSchema)
+      ..free(cTableName)
+      ..free(cTableType);
+
+    return result;
+  }
+
+  /// Helper function to get a column value from the result set
+  String _getColumnValue(SQLHSTMT hStmt, int colIndex, ColumnType columnType) {
+    final valueLength = calloc.allocate<SQLLEN>(sizeOf<SQLLEN>());
+    final value = calloc.allocate<Uint16>(sizeOf<Uint16>() * columnType.size!);
+
+    tryOdbc(
+      _sql.SQLGetData(
+        hStmt,
+        colIndex,
+        columnType.type!,
+        value.cast(),
+        columnType.size!,
+        valueLength,
+      ),
+      handle: hStmt,
+    );
+
+    if (valueLength.value == SQL_NULL_DATA) {
+      return '';
+    }
+
+    // Convert the value to a Dart string, removing trailing zeros
+    final charCodes = value.asTypedList(valueLength.value).toList();
+    charCodes.removeWhere((e) => e == 0);
+
+    final result = String.fromCharCodes(charCodes);
+
+    calloc
+      ..free(value)
+      ..free(valueLength);
+
+    return result;
+  }
+
   /// Execute a query
   /// The [query] parameter is the SQL query to execute.
   /// This function will return a list of maps where each map represents a row

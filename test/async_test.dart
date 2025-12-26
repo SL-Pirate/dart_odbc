f7ecb259cc32f0b5ddeaf7c7dae62346fd0fe816
Isira Herath <isiraherath626@gmail.com>
2025-12-26 17:06:54 +0530

    Added new test for parallel execution of queries/commands

---
+++ test/async_test.dart
@@ -0,0 +1,28 @@
+import 'package:test/test.dart';
+
+import 'test_helper.dart';
+
+void main() {
+  final helper = TestHelper();
+
+  setUpAll(helper.initialize);
+
+  tearDownAll(helper.disconnect);
+
+  test('parallel async query test', () async {
+    final futures = List.generate(10, (index) {
+      return helper.query(
+      'SELECT ? AS index_value;',
+      params: [index],
+      );
+    });
+
+    final results = await Future.wait(futures);
+
+    for (var i = 0; i < results.length; i++) {
+      final result = results[i];
+      expect(result.length, 1);
+      expect(result[0]['index_value'], i.toString());
+    }
+  });
+}
